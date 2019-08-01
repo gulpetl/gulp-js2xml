89d98bd57b4af3f5ebcf9bf4417a5280385b8c5c
Peter Lambert <peterl@sbcotton.com>
2019-07-31 21:00:24 -0500

    1.0.3: Cleanup/clarify - point to new companion plugin gulp-xmltojson - add round trip to demonstrate json->xml->json capabilities; set formatting on test files to match originals - cleanup/clarify gulpfile.ts

--- debug/gulpfile.ts
+++ debug/gulpfile.ts
@@ -1,39 +1,38 @@
 let gulp = require('gulp')
-import {jsontoxml} from '../src/plugin'
-import {xmltojson} from 'gulp-xmltojson'
+import { jsontoxml } from '../src/plugin'
+import { xmltojson } from 'gulp-xmltojson'
 import * as loglevel from 'loglevel'
-//import { strict } from 'assert';
 const log = loglevel.getLogger('gulpfile')
 log.setLevel((process.env.DEBUG_LEVEL || 'warn') as loglevel.LogLevelDesc)
-var options = {compact: true, spaces: 4};
-var options2 = {compact:true};
-const pkginfo = require('pkginfo')(module); // project package.json info into module.exports
+
+require('pkginfo')(module); // project package.json info into module.exports
 const PLUGIN_NAME = module.exports.name;
 
 //the plugin will be called here
-    export function xml (callback:any) {
-      gulp.src('../testdata/*.json')
-        .pipe(jsontoxml(options))
-        .on('data', function (file:any) {
-          console.log('Done creating ' + file.basename)
-      }) 
-        .pipe(gulp.dest('../testdata/processed'))
-        callback();
-    }
-
- //the roundtrip back to 
- export function json(callback: any) {
-  gulp.src('../testdata/processed/*.xml')
-  .pipe(xmltojson(options2))
-  .on('data', function (file:any) {
-    console.log('Done creating ' + file.basename)
-})  
-  .pipe(gulp.dest('../testdata/processed/round trip'))
- .on('end', function () {
-    log.info('gulp task complete')
-  })
+export function calljsontoxml(callback: any) {
+  gulp.src('../testdata/*.json')
+    .pipe(jsontoxml({ compact: true, spaces: 4 }))
+    .on('data', function (file: any) {
+      console.log('Done creating ' + file.basename)
+    })
+    .pipe(gulp.dest('../testdata/processed'))
   callback();
-};   
-    
+}
 
-exports.default = gulp.series(xml,json)
+//the roundtrip back to json using our companion plugin gulp-xmltojson
+export function callxmltojson(callback: any) {
+  gulp.src('../testdata/processed/*.xml')
+    .pipe(xmltojson({ compact: true, spaces: 4 }))
+    .on('data', function (file: any) {
+      console.log('Done creating ' + file.basename)
+    })
+    .pipe(gulp.dest('../testdata/processed/round trip'))
+    .on('end', function () {
+      log.info('gulp task complete')
+    })
+  callback();
+};
+
+
+exports.default = gulp.series(calljsontoxml)
+exports.roundtrip = gulp.series(calljsontoxml, callxmltojson)
